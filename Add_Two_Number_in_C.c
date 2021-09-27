d1d7529ec6193d84e74b4a15df1a176745755dbd
Ayushman's Code <91487455+apycoder@users.noreply.github.com>
2021-09-27 21:53:21 +0530

    Program Complete

--- Add_Two_Number_in_C.c
+++ Add_Two_Number_in_C.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    // Print to Enter Two Numbers.
+    int N1, N2;
+    printf("Enter 1st Number: ");
+    scanf("%d", &N1);
+    printf("Enter 2nd Number: ");
+    scanf("%d", &N2);
+    printf("%d + %d = %d", N1, N2, N1 + N2);
+    return 0;
+}
